96e89ad3a03565661f731d0dc6be10398c8dad48
Isshin Toyonaga <isshintakatsusaido@gmail.com>
2016-08-16 14:18:39 +0800

    add robot class

--- index.php
+++ index.php
@@ -8,4 +8,23 @@
 
  	 $result=add(1+1);
  	echo $result;
+
+ 	/**
+ 	* 
+ 	*/
+ 	class Robot
+ 	{
+ 		private $name;
+
+ 		function setName($name){
+ 			$this->name=$name;
+ 		}
+ 		function getName(){
+ 			return $this->name;
+ 		}
+ 	}
+ 	$robot = new Robot();
+ 	$robot->setName('isshin');
+ 	$isshin = $robot->getName();
+ 	echo $isshin;
  ?>
